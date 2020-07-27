6ea7d688b6d47bdd7fa95564a5423d7acc69b9b2
João L. S. Silva <joao.silva@easysoft.com.br>
2020-07-27 08:56:23 -0300

    add EXPIRED_DOC

--- frontend/antifraude.ts
+++ frontend/antifraude.ts
@@ -10,4 +10,15 @@ export const MISSING_EXP_DATE: ZiroMessageData = {
         resolution: "Envie uma nova imagem do documento."
     },
     internalDescription: "A resposta da nextcode não contém uma data de expedição válida para esse documento"
+},
+EXPIRED_DOC: ZiroMessageData = {
+    code: "0002",
+    type: "destructive",
+    title: "Documento expirado",
+    userDescription: {
+        short: "A data do documento é maior do que a permitida.",
+        long: "O documento possui data de expedição maior do que a permitida pela Ziro.",
+        resolution: "Envie uma imagem de um documento mais recente."
+    },
+    internalDescription: "O algoritmo de antifraude detectou que a data de expedição excede aquilo que foi estipulado no código, consulte um dev para saber mais"
 }
