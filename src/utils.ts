759746d4b4d3d0380cdad68db2a13c615e4e98a1
João L. S. Silva <joao.silva@easysoft.com.br>
2020-07-28 23:23:43 -0300

    direct casting because bit is still complaining

--- src/utils.ts
+++ src/utils.ts
@@ -4,14 +4,16 @@ type PP = { [key: string]: ZiroMessageData&ZiroPromptMessageData }
 type RP<V> = { [K in keyof V]: ZiroPromptMessage<K> }
 
 export const createClassPromptObjects = function<V extends PP>(pack: V): RP<V> {
-    return Object.entries(pack).reduce((acc,[n,m]) => ({ ...acc, [n]: new ZiroPromptMessage({ ...m, name: n }) }),{} as RP<V>)
+    return (<[string,ZiroMessageData&ZiroPromptMessageData][]>(<any>Object).entries(pack))
+        .reduce((acc,[n,m]) => ({ ...acc, [n]: new ZiroPromptMessage({ ...m, name: n }) }),{} as RP<V>)
 }
 
 type PW = { [key: string]: ZiroMessageData&ZiroWaitingMessageData }
 type RW<V> = { [K in keyof V]: ZiroWaitingMessage<K> }
 
 export const createClassWaitingObjects = function<V extends PW>(pack: V): RW<V> {
-    return Object.entries(pack).reduce((acc,[n,m]) => ({ ...acc, [n]: new ZiroWaitingMessage({ ...m, name: n }) }),{} as RW<V>)
+    return (<[string,ZiroMessageData&ZiroPromptMessageData][]>(<any>Object).entries(pack))
+        .reduce((acc,[n,m]) => ({ ...acc, [n]: new ZiroWaitingMessage({ ...m, name: n }) }),{} as RW<V>)
 }
 
 export type WMessage = ZiroMessageData & ZiroWaitingMessageData
