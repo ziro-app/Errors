4156dbc37d00ec878eab62f4cad5270e1723e303
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-03 16:07:52 -0300

    fix sintax so bit compiler wont complain

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from  "./ZiroMessage"
 export * from "./ZiroPromptMessage"
 export * from "./ZiroWaitingMessage"
 
-export * as catalogo from "./catalogo"
-export * as nextcode from "./nextcode"
+import * as catalogo from "./catalogo"
+import * as nextcode from "./nextcode"
+export { catalogo, nextcode }
