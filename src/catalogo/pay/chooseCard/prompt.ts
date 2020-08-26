0c5faaa4357c52f601324a2ca52ed50460d75b25
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-26 16:27:05 -0300

    create delete card message

--- src/catalogo/pay/chooseCard/prompt.ts
+++ src/catalogo/pay/chooseCard/prompt.ts
@@ -1,12 +1,22 @@
 import { ChooseCardPromptData } from "../../../../codes";
 
-export const CANNOT_DELETE_FROM_FIRESTORE: ChooseCardPromptData<"0100"> = {
-	code: "0100",
-	type: "destructive",
-	title: "CANNOT_DELETE_FROM_FIRESTORE",
-	illustration: "paymentError",
-	userDescription: "CANNOT_DELETE_FROM_FIRESTORE",
-	userResolution: "CANNOT_DELETE_FROM_FIRESTORE",
-	internalDescription: "CANNOT_DELETE_FROM_FIRESTORE",
-	additionalData: undefined,
-};
+export const DELETE_CARD: ChooseCardPromptData<"0100"> = {
+		code: "0100",
+		type: "destructive",
+		title: "DELETE_CARD",
+		illustration: "paymentError",
+		userDescription: "DELETE_CARD",
+		userResolution: "DELETE_CARD",
+		internalDescription: "DELETE_CARD",
+		additionalData: undefined,
+	},
+	CANNOT_DELETE_FROM_FIRESTORE: ChooseCardPromptData<"0101"> = {
+		code: "0101",
+		type: "destructive",
+		title: "CANNOT_DELETE_FROM_FIRESTORE",
+		illustration: "paymentError",
+		userDescription: "CANNOT_DELETE_FROM_FIRESTORE",
+		userResolution: "CANNOT_DELETE_FROM_FIRESTORE",
+		internalDescription: "CANNOT_DELETE_FROM_FIRESTORE",
+		additionalData: undefined,
+	};
