988cd5764bf3cd86210afc061d2632f30d53400e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-08-25 20:32:38 -0700

    review messages

--- src/catalogo/antifraude/common/prompt.ts
+++ src/catalogo/antifraude/common/prompt.ts
@@ -6,9 +6,9 @@ NO_IMAGE: CommonAntifraudePromptData<"0075"> = {
     type: "destructive",
     title: "É preciso enviar uma imagem",
     illustration: "paymentError",
-    userDescription: "Não é possível passar para a próxima etapa sem enviar uma imagem para análise.",
+    userDescription: "",
     userResolution: "Tire uma foto ou escolha uma imagem salva em seu aparelho.",
-    internalDescription: "O usuário clicou em próximo sem ter tirado foto alguma.",
+    internalDescription: "O usuário clicou em próximo sem ter enviado foto alguma.",
     additionalData: undefined
 },
 CLOSE: CommonAntifraudePromptData<"0076"> = {
@@ -34,20 +34,20 @@ TOO_MANY_ATTEMPTS: CommonAntifraudePromptData<"0077"> = {
 CANNOT_SAVE_TO_FIRESTORE: CommonAntifraudePromptData<"0078"> = {
     code: "0078",
     type: "destructive",
-    title: "CANNOT_SAVE_TO_FIRESTORE",
+    title: "Ocorreu um erro na solicitação",
     illustration: "paymentError",
-    userDescription: "Parece que o processo falhou muitas vezes seguidas.",
-    userResolution: "Contate o suporte para continuar.",
-    internalDescription: "O usuário tentou realizar o mesmo procedimento muitas vezes sem sucesso e por isso foi impossibilitado de continuar.",
+    userDescription: "",
+    userResolution: "Tente novamente após um tempo. Se o erro persistir contate o suporte.",
+    internalDescription: "A requisição para a base de dados no Firebase gerou erro. Contate os devs",
     additionalData: undefined
 },
-CANNOT_UPLOAD_PICTURE_DO_STORAGE: CommonAntifraudePromptData<"0079"> = {
+CANNOT_UPLOAD_PICTURE_TO_STORAGE: CommonAntifraudePromptData<"0079"> = {
     code: "0079",
     type: "destructive",
-    title: "CANNOT_SAVE_TO_FIRESTORE",
+    title: "Ocorreu um erro na solicitação",
     illustration: "paymentError",
-    userDescription: "Parece que o processo falhou muitas vezes seguidas.",
-    userResolution: "Contate o suporte para continuar.",
-    internalDescription: "O usuário tentou realizar o mesmo procedimento muitas vezes sem sucesso e por isso foi impossibilitado de continuar.",
+    userDescription: "",
+    userResolution: "Tente novamente após um tempo. Se o erro persistir contate o suporte.",
+    internalDescription: "A requisição para o storage de imagens no Firebase gerou erro. Contate os devs",
     additionalData: undefined
 }
