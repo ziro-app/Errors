d3ec8384ab196b6cd67827b689217f0c5ad6246b
João L. S. Silva <joao.silva@easysoft.com.br>
2020-08-27 11:52:09 -0300

    create prompt message for selfie

--- src/catalogo/antifraude/biometry/index.ts
+++ src/catalogo/antifraude/biometry/index.ts
@@ -1,7 +1,9 @@
 import { createPromptClassObject, createWaitingClassObject } from "../../../utils";
 
-// import * as promptMessages from "./prompt"
-// export const prompt = createPromptClassObject(promptMessages)
+import * as promptMessages from "./prompt";
 
-// import * as waitingMessages from "./waiting"
-// export const waiting = createWaitingClassObject(waitingMessages)
+import * as waitingMessages from "./waiting";
+
+export const prompt = createPromptClassObject(promptMessages);
+
+export const waiting = createWaitingClassObject(waitingMessages);
