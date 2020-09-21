35f7f55eac379bddec1ac62efd784abd5c616ed2
João L. S. Silva <joao.silva@easysoft.com.br>
2020-09-21 08:07:41 -0300

    creating rede prompt class object

--- src/zoop/index.ts
+++ src/zoop/index.ts
@@ -1,5 +1,7 @@
 import { createPromptClassObject } from "../utils";
 
 import * as promptMessages from "./prompt";
+import * as redePromptMessages from "./redePrompt";
 
 export const prompt = createPromptClassObject(promptMessages);
+export const rede = createPromptClassObject(redePromptMessages);
