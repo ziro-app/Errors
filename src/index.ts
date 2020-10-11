5e1edeb86c51e2e2b0f8ff310588f3cf2aa5e0e9
João L. S. Silva <joao.silva@easysoft.com.br>
2020-10-11 15:48:47 -0300

    using cors-anywhere

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,12 @@ export * from "./ZiroMessage";
 export * from "./ZiroPromptMessage";
 export * from "./ZiroWaitingMessage";
 
-axios.get("https://registry.npmjs.org/ziro-messages").then(({ data }) => {
-	const current = packageJson.version;
-	const { latest } = data["dist-tags"];
-	// eslint-disable-next-line no-console
-	if (current !== latest) console.error("YOU ARE RUNNING AN OUTDATED VERSION OF ZIRO-MESSAGES, PLEASE UPDATE");
-});
+axios
+	.get("https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/ziro-messages")
+	.then(({ data }) => {
+		const current = packageJson.version;
+		const { latest } = data["dist-tags"];
+		// eslint-disable-next-line no-console
+		if (current !== latest) console.error("YOU ARE RUNNING AN OUTDATED VERSION OF ZIRO-MESSAGES, PLEASE UPDATE");
+	})
+	.catch(() => null);
