6f839951fb9e0423210102a5a2e3492904ab16b0
João L. S. Silva <joao.silva@easysoft.com.br>
2020-10-11 14:56:11 -0300

    show error if package is outdated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+import { packument } from "pacote";
+import packageJson from "../package.json";
+
 export * from "./ZiroMessage";
 export * from "./ZiroPromptMessage";
 export * from "./ZiroWaitingMessage";
+
+packument("ziro-messages").then((data) => {
+	const current = packageJson.version;
+	const { latest } = data["dist-tags"];
+	// eslint-disable-next-line no-console
+	if (current !== latest) console.error("YOU ARE RUNNING AN OUTDATED VERSION OF ZIRO-MESSAGES, PLEASE UPDATE");
+});
