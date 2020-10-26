82857711b4a240d912921548325ec367a35a8ec8
João L. S. Silva <joao.silva@easysoft.com.br>
2020-10-26 09:14:48 -0300

    add no installments message

--- src/catalogo/pay/checkout/prompt.ts
+++ src/catalogo/pay/checkout/prompt.ts
@@ -1,12 +1,22 @@
 import { CheckoutPromptData } from "../../../../codes";
 
 export const PAYMENT_SUCCESS: CheckoutPromptData<"0150"> = {
-	code: "0150",
-	type: "success",
-	title: "Pagamento realizado",
-	illustration: "paymentSuccess",
-	userDescription: "O pagamento foi realizado com sucesso",
-	userResolution: "",
-	internalDescription: "O pagamento foi bem sucedido",
-	additionalData: undefined,
-};
+		code: "0150",
+		type: "success",
+		title: "Pagamento realizado",
+		illustration: "paymentSuccess",
+		userDescription: "O pagamento foi realizado com sucesso",
+		userResolution: "",
+		internalDescription: "O pagamento foi bem sucedido",
+		additionalData: undefined,
+	},
+	NO_INSTALLMENTS: CheckoutPromptData<"0151"> = {
+		code: "0151",
+		type: "destructive",
+		title: "Nenhum parcelamento",
+		illustration: "noData",
+		userDescription: "Nenhum parcelamento foi escolhido.",
+		userResolution: "Para continuar é necessário escolher o parcelamento.",
+		internalDescription: "O usuario tentou pagar antes de escolher o parcelamento",
+		additionalData: undefined,
+	};
