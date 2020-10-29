b200d6e7b6fb3d088a6b3f1277f1ea1d91b69a5e
João L. S. Silva <joao.silva@easysoft.com.br>
2020-10-28 21:09:15 -0300

    create generic message

--- src/catalogo/index.ts
+++ src/catalogo/index.ts
@@ -1,3 +1,18 @@
 import * as antifraude from "./antifraude";
+import { CatalogUnknownData } from "../../codes";
+import { createPromptClassObject } from "../utils";
+
+const UNKNOWN_ERROR: CatalogUnknownData<"0000"> = {
+	code: "0000",
+	type: "destructive",
+	title: "Ocorreu um erro desconhecido",
+	illustration: "noData",
+	userDescription:
+		"Nossos desenvolvedores já estão tentando encontrar uma solução para que isso não aconteça novamente.",
+	userResolution: "Se o erro persistir entre em contato com nosso suporte.",
+	internalDescription: "Ocorreu um erro que não está mapeado",
+	additionalData: undefined,
+};
 
 export { antifraude };
+export const prompt = createPromptClassObject({ UNKNOWN_ERROR });
