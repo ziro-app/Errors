37a8c50869ac7b301553efe7adbad4ffbc117d8d
João L. S. Silva <joao.silva@easysoft.com.br>
2020-11-11 19:36:13 -0300

    fixes

--- src/catalogo/camera/index.ts
+++ src/catalogo/camera/index.ts
@@ -1,6 +1,5 @@
-import { createPromptClassObject, createWaitingClassObject } from "../../../utils";
-import { GetPromptCode, GetPromptData, PromptMessage } from "../../../ZiroPromptMessage";
-import { GetWaitingCode, GetWaitingData, WaitingMessage } from "../../../ZiroWaitingMessage";
+import { createPromptClassObject } from "../../utils";
+import { GetPromptCode, GetPromptData, PromptMessage } from "../../ZiroPromptMessage";
 
 import * as promptMessages from "./prompt";
 
@@ -10,10 +9,7 @@ export type CommonPromptCollection = typeof prompt;
 export type GetCommonPromptCode<N extends keyof CommonPromptCollection> = GetPromptCode<CommonPromptCollection, N>;
 export type GetCommonPromptData<N extends keyof CommonPromptCollection> = GetPromptData<CommonPromptCollection, N>;
 export type CommonPromptMessage<N extends keyof CommonPromptCollection, D = GetCommonPromptData<N>> = PromptMessage<
-    CommonPromptCollection,
-    N,
-    D
+	CommonPromptCollection,
+	N,
+	D
 >;
-
-// import * as waitingMessages from "./waiting"
-// export const waiting = createWaitingClassObject(waitingMessages)
