c9f0e50afa6af10c5e144cb8a85250952dbe5cee
João L. S. Silva <joao.silva@easysoft.com.br>
2020-11-19 17:36:39 -0300

    strip the checking code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import packageJson from "../package.json";
-
 export * from "./ZiroMessage";
 export * from "./ZiroPromptMessage";
 export * from "./ZiroWaitingMessage";
-
-fetch("https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/ziro-messages", { mode: "no-cors" })
-	.then((r) => r.json())
-	.then((data) => {
-		const current = packageJson.version;
-		const { latest } = data["dist-tags"];
-		if (current < latest)
-			// eslint-disable-next-line no-console
-			console.error("YOU ARE RUNNING AN OUTDATED VERSION OF ZIRO-MESSAGES, PLEASE UPDATE", { current, latest });
-	})
-	.catch(() => null);
