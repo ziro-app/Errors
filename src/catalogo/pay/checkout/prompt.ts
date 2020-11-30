d326ead1e13f5de42dee8cca2e3e7b0685dabe9d
Alessandro <vyseclown@gmail.com>
2020-11-30 16:15:03 -0300

    chore: adding ERROR_CREATING_SPLIT_OBJECT to prompt.ts

--- src/catalogo/pay/checkout/prompt.ts
+++ src/catalogo/pay/checkout/prompt.ts
@@ -19,4 +19,14 @@ export const PAYMENT_SUCCESS: CheckoutPromptData<"0150"> = {
 		userResolution: "Para continuar é necessário escolher o parcelamento.",
 		internalDescription: "O usuario tentou pagar antes de escolher o parcelamento",
 		additionalData: undefined,
+	},
+	ERROR_CREATING_SPLIT_OBJECT: CheckoutPromptData<"0152"> = {
+		code: "0152",
+		type: "destructive",
+		title: "Ocorreu um erro no pagamento",
+		illustration: "noData",
+		userDescription: "Fique tranquilo, você não foi cobrado!",
+		userResolution: "Aconteceu um erro de split, contate o suporte!",
+		internalDescription: "As porcentagens do split de pagamento não foram encontradas",
+		additionalData: undefined,
 	};
