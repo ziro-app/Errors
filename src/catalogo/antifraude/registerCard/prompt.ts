a67ee622949f18a69471c7edfe857d8d5dde5cbe
João L. S. Silva <joao.lucas.ss@hotmail.com>
2020-12-11 21:19:05 -0300

    add wrong transaction value message

--- src/catalogo/antifraude/registerCard/prompt.ts
+++ src/catalogo/antifraude/registerCard/prompt.ts
@@ -1,1 +1,12 @@
 import { RegisterCardPromptData } from "../../../../codes";
+
+export const WRONG_TRANSACTION_VALUE: RegisterCardPromptData<"0001"> = {
+	code: "0001",
+	type: "destructive",
+	title: "Valor incorreto",
+	illustration: "paymentError",
+	userDescription: "O valor informado não corresponde ao valor da transação enviada",
+	userResolution: "Tente novamente, inserindo o valor que consta em sua fatura.",
+	internalDescription: "O usuário está registrando um novo cartão",
+	additionalData: undefined,
+};
