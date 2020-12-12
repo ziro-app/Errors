b883e66ab6f643e716e5050c6cefcd69dd402a02
João L. S. Silva <joao.lucas.ss@hotmail.com>
2020-12-11 21:36:08 -0300

    add confirm transaction message

--- src/catalogo/antifraude/registerCard/prompt.ts
+++ src/catalogo/antifraude/registerCard/prompt.ts
@@ -1,12 +1,23 @@
 import { RegisterCardPromptData } from "../../../../codes";
 
 export const WRONG_TRANSACTION_VALUE: RegisterCardPromptData<"0001"> = {
-	code: "0001",
-	type: "destructive",
-	title: "Valor incorreto",
-	illustration: "paymentError",
-	userDescription: "O valor informado não corresponde ao valor da transação enviada",
-	userResolution: "Tente novamente, inserindo o valor que consta em sua fatura.",
-	internalDescription: "O usuário está registrando um novo cartão",
-	additionalData: undefined,
-};
+		code: "0001",
+		type: "destructive",
+		title: "Valor incorreto",
+		illustration: "paymentError",
+		userDescription: "O valor informado não corresponde ao valor da transação enviada",
+		userResolution: "Tente novamente, inserindo o valor que consta em sua fatura.",
+		internalDescription: "O usuário colocou o valor errado.",
+		additionalData: undefined,
+	},
+	CONFIRM_TRANSACTION: RegisterCardPromptData<"0002"> = {
+		code: "0002",
+		type: "destructive",
+		title: "Uma transação será enviada",
+		illustration: "paymentError",
+		userDescription:
+			"Uma transação de pequeno valor será enviada e estornada. Utilize o valor para validar seu cartão.",
+		userResolution: "Caso seu cartão expire após uma compra, utilize outro método de validação.",
+		internalDescription: "O usuário está requisitando uma nova transação",
+		additionalData: undefined,
+	};
